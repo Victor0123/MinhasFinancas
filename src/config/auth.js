83215c2b36ca4ec33baa1ed01c87342fa8c85dea
Victor André Rossini de Souza <victor.rossini01@hotmail.com>
2021-02-09 17:03:08 -0300

    Login/update e autenticaçao de usuarios

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '9ad5abb952a328a91fc736d43a5767ef',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,18 @@ import { Router } from 'express';
 
 import LancamentoController from './app/controllers/LancamentoController';
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 routes.post('/lancamento', LancamentoController.store);
 routes.get('/lancamentos/:data', LancamentoController.index);

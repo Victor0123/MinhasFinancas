3cb3b9ef8c5fe836cb963c920c5b959c5ca35bce
Victor André Rossini de Souza <victor.rossini01@hotmail.com>
2021-02-09 14:32:59 -0300

    Cria usuario e ralacionamento users/lancamentos

--- src/app/models/Lancamento.js
+++ src/app/models/Lancamento.js
@@ -14,6 +14,10 @@ class Lancamento extends Model {
         sequelize,
       }
     );
+    return this;
+  }
+  static associate(models) {
+    this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
 import LancamentoController from './app/controllers/LancamentoController';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.post('/lancamento', LancamentoController.store);
+routes.post('/users', UserController.store);
 
+routes.post('/lancamento', LancamentoController.store);
 routes.get('/lancamentos/:data', LancamentoController.index);
 
 export default routes;

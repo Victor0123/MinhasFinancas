42d6d5f14f6b109a4b14610962c4d59590615c9c
Victor André Rossini de Souza <victor.rossini01@hotmail.com>
2021-02-15 15:10:22 -0300

    Ainda tentando deploy

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
-import 'dotenv/config.js';
+import 'dotenv/config';
 
 import express from 'express';
 import cors from 'cors';
-import routes from './routes.js';
+import routes from './routes';
 
 import './database';
 

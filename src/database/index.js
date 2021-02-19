72c263fced4c42379fb4df9942179397c2efc5fa
Victor André Rossini de Souza <victor.rossini01@hotmail.com>
2021-02-19 10:59:45 -0300

    Erro sequelize

--- src/database/index.js
+++ src/database/index.js
@@ -13,7 +13,7 @@ class Database {
   }
 
   init() {
-    console.log('dbURL:', process.env.DATABASE_URL);
+    console.log('dbURL:', databaseConfig);
     this.connection = new Sequelize(databaseConfig);
 
     models

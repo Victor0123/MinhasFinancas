75f2ceb7a6e4f2c252fc128f36e0f3bd2b861130
Victor André Rossini de Souza <victor.rossini01@hotmail.com>
2021-02-22 12:27:04 -0300

    Erro sequelize

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 require('dotenv/config');
 
 module.exports = {
-  use_env_variable: process.env.DATABASE_URL,
+  // use_env_variable: process.env.DATABASE_URL,
   dialect: 'postgres',
   dialectOptions: {
     ssl: {

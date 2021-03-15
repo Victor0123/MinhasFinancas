59114cc32c74f745f01b550d1907d6d9224cb945
Victor André Rossini de Souza <victor.rossini01@hotmail.com>
2021-03-15 13:02:11 -0300

    Update cors url

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ class App {
   }
 
   middlewares() {
-    this.server.use(cors({ origin: 'https://minhas-financas-front-end.herokuapp.com' }));
+    this.server.use(cors({ origin: 'https://minhasfinancas20.herokuapp.com' }));
     this.server.use(express.json());
   }
 

ba026532e78f3ea7b85f1a4aec5b5cbebf651f2b
Victor André Rossini de Souza <victor.rossini01@hotmail.com>
2021-09-14 23:26:28 -0300

    Add migration phone number to users

---
+++ src/database/migrations/20210915012617-add-phoneNumber-users.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('users', 'phone_number', {
+      type: Sequelize.STRING,
+      allowNull: true,
+      unique: true,
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('users', 'phone_number');
+  }
+};
